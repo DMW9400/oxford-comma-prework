54d1b227942f5deec92edbbb480c3398e059e4ac
David Welch <dmw9400@gmail.com>
2017-11-10 16:00:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,17 +4,11 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length > 2
-    # i = 0
-    # return_string = ""
-    # final_string = "and " + array[-1] +
-    # while i < array.length-1
-    #   return_string += array[i]+ ", "
-    #   i+=1
-    # end
-    # return_string = return_string + final_string
-    # return return_string
+    array2 = []
+    array2.push(array[0...-1])
+    array2.join(", ")
     
 
-    
+
   end
 end
